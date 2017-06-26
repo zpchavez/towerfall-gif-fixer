bccffc6a7d5527b77f478c34a70cf53a7f005342
Zachary Chavez <zpchavez@gmail.com>
2017-06-25 17:23:16 -0700

    Get bad frames way faster

--- fixer.js
+++ fixer.js
@@ -31,54 +31,49 @@ glob(inputGlob, (err, files) => {
   )
 });
 
-const isBadFrame = function(framePath, width, height) {
-  return new Promise((outerResolve, outerReject) => {
-    const promises = [];
-    for (let x = 0; x <= width; x += 100) {
-      promises.push(
-        new Promise((innerResolve, innerReject) => {
-          im.convert(
-            [
-              framePath,
-              '-crop',
-              `1x1+${x}+${height-1}`,
-              'txt:-'
-            ],
-            (err, stdout, stderr) => {
-              if (err) {
-                console.log(err);
-                return outerReject(err);
-              }
-              innerResolve(stdout.indexOf('#000000') > -1);
-            });
-        }));
-    }
-    return Promise.all(promises).then(results => {
-      outerResolve(results.indexOf(false) === -1);
-    });
-  });
-}
-
 const getBadFramesForFile = function(inputFile) {
-  return new Promise(resolve => {
+  return new Promise((resolve, reject) => {
     im.identify([inputFile], (err, output) => {
       let width, height, frameCount;
       const outputLines = output.split("\n").filter(v => !!v);
       frameCount = outputLines.length;
       width = outputLines[0].match(/GIF (\d+)x/)[1];
       height = outputLines[0].match(/GIF \d+x(\d+)/)[1];
-      const badFramePromises = [];
-      for (let frame = 0; frame < frameCount; frame += 1) {
-      // for (let frame = 15; frame < 16; frame +=1 ) {
-        badFramePromises.push(isBadFrame(`${inputFile}[${frame}]`, width, height));
+      const xPositions = [];
+      for (let i = 0; i < width; i += 100) {
+        xPositions.push(i);
       }
-      return Promise.all(badFramePromises).then(results => {
+      Promise.all(xPositions.map(x => {
+        return new Promise((resolve) =>
+          im.convert([inputFile, '-crop', `1x1+${x}+${height-1}`, 'txt:-'], (err, stdout, stderr) => {
+            if (err) {
+              reject(err);
+            }
+
+            resolve(
+              stdout.split("\n")
+                .filter(v => v && v.indexOf('#') !== 0)
+                .map(v => v.indexOf('#000000') > -1)
+            );
+          }))
+      })).then(positionData => {
+        const goodFrames = [];
         const badFrames = [];
-        results.forEach((bad, frame) => {
-          if (bad) {
+        for (let frame = 0; frame < positionData[0].length; frame += 1) {
+          let isGood = false;
+          positionData.forEach(positionDatum => {
+            if (isGood) {
+              return;
+            }
+            if (!positionDatum[frame]) {
+              isGood = true;
+              goodFrames.push(frame);
+            }
+          });
+          if (!isGood) {
             badFrames.push(frame);
           }
-        })
+        }
         resolve(badFrames);
       })
     });
@@ -90,22 +85,3 @@ const fix = function(inputFile, outputFile) {
     console.log('bad frames', badFrames);
   });
 }
-
-
-// im.convert(['./gifs/replay566.gif[0]', '-crop', '1x1+0+479', 'txt:-'], (err, stdout, stderr) => {
-//   console.log(stdout);
-// });
-// im.convert(['./gifs/replay566.gif[0]', '-crop', '1x1+100+479', 'txt:-'], (err, stdout, stderr) => {
-//   console.log(stdout);
-// });
-// im.convert(['./gifs/replay566.gif[0]', '-crop', '1x1+200+479', 'txt:-'], (err, stdout, stderr) => {
-//   console.log(stdout);
-// });
-// im.convert(['./gifs/replay566.gif[0]', '-crop', '1x1+300+479', 'txt:-'], (err, stdout, stderr) => {
-//   console.log(stdout);
-// });
-
-
-// im.convert(['./gifs/replay566-14.png', '-crop', '1x1+0+479', 'txt:-'], (err, stdout, stderr) => {
-//   console.log(stdout);
-// });
